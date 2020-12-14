e978ea5b738a8f9eb22f93467eeec82bc5971805
kitamura-hub <masato1999824@icloud.com>
2020-12-14 22:44:05 +0900

    Added error handling process

--- src/main.js
+++ src/main.js
@@ -9,5 +9,11 @@ const filePath = commander.args[0];
 
 // ファイルを非同期で読み込む(utf-8形式で)
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
-    console.log(file);
+  if(err) {
+    console.error(err.message);
+    // 終了ステータス1(一般的なエラー)としてプロセスを終了する
+    process.exit(1);
+    return;
+  }
+  console.log(file);
 });
