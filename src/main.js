131f621d8e3ee53e2b03560c675db10f855eff02
kitamura-hub <masato1999824@icloud.com>
2020-12-14 22:38:47 +0900

    Completed implementation of file reading

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 const commander = require("commander");
 // fsモジュールをインポート
 const fs = require("fs");
-// コマンドライン引数をcommanderでパースする
-commander.parse(process.argv);
 
-// ファイルパスをprogram.args配列から取り出す
+// コマンドライン引数からファイルパスを取得する
+commander.parse(process.argv);
 const filePath = commander.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む(utf-8形式で)
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    console.log(file);
+});
